7cf3e97c30edaee08737c0002f374d3590ef9497
Marcus Ha <68570454+marcusha429@users.noreply.github.com>
2025-10-07 14:43:53 -0700

    Time: 0 ms (100%), Space: 17.7 MB (86.8%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,24 +1,40 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        def sum_of_square_digit(num):
-            sosd = 0
-            while num != 0:
-                digit = num % 10
-                sosd += digit**2
-                num = num // 10
-            return sosd
+        # def sum_of_square_digit(num):
+        #     sosd = 0
+        #     while num != 0:
+        #         digit = num % 10
+        #         sosd += digit**2
+        #         num = num // 10
+        #     return sosd
         
-        if n == 1:
-            return True
+        # if n == 1:
+        #     return True
         
-        slow_ptr = n
-        fast_ptr = sum_of_square_digit(n)
+        # slow_ptr = n
+        # fast_ptr = sum_of_square_digit(n)
 
-        while slow_ptr != fast_ptr:
-            if fast_ptr == 1:
-                return True
-            slow_ptr = sum_of_square_digit(slow_ptr)
-            fast_ptr = sum_of_square_digit(sum_of_square_digit(fast_ptr))
+        # while slow_ptr != fast_ptr:
+        #     if fast_ptr == 1:
+        #         return True
+        #     slow_ptr = sum_of_square_digit(slow_ptr)
+        #     fast_ptr = sum_of_square_digit(sum_of_square_digit(fast_ptr))
             
-        return False
+        # return False
+
+        hashmap = set()
+        while n != 1:
+            if n in hashmap:
+                return False
+            hashmap.add(n)
+
+            sosd = 0
+            temp = n
+            while temp != 0:
+                digit = temp % 10
+                sosd += digit ** 2
+                temp = temp // 10
+            n = sosd
+        return True
+            
         
