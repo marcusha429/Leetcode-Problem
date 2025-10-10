ac991826e10bd4a7fe2a470ab35535389c7d1d98
Marcus Ha <68570454+marcusha429@users.noreply.github.com>
2025-10-10 15:16:32 -0700

    Time: 3 ms (51.78%), Space: 17.6 MB (92.84%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,10 +1,19 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        s = list(s)
-        for letter in t:
-            if letter not in s:
-                return letter
-            s.remove(letter)
+        # s = list(s)
+        # for letter in t:
+        #     if letter not in s:
+        #         return letter
+        #     s.remove(letter)
+
+        hashmap = {}
+        for letter_s in s:
+            hashmap[letter_s] = hashmap.get(letter_s, 0) + 1
+
+        for letter_t in t:
+            hashmap[letter_t] = hashmap.get(letter_t, 0) - 1
+            if hashmap[letter_t] < 0:
+                return letter_t
 
             
             
