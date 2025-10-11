d244780da07b9fe8275b1f08ace5a5e69f1a9fd6
Marcus Ha <68570454+marcusha429@users.noreply.github.com>
2025-10-11 14:42:45 -0700

    Time: 3 ms (42.08%), Space: 18 MB (39.67%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -2,15 +2,12 @@ class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         hashmap = {}
         stack = []
-        ans = []
         for num2 in nums2:
             while stack and num2 > stack[-1]:
                 smaller_element = stack.pop()
                 hashmap[smaller_element] = num2
             stack.append(num2)
         
-        for num1 in nums1:
-            ans.append(hashmap.get(num1, -1))
-        return ans
+        return [hashmap.get(num1, -1) for num1 in nums1]
 
             
